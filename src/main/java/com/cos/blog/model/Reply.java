e1a6986c88d1d3b402ea925af49437d7f80790a4
Sumin-yun <sumin.yun73@gmail.com>
2022-07-28 00:23:07 +0900

    git add

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -1,6 +1,7 @@
 package com.cos.blog.model;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +9,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.cos.blog.dto.ReplySaveRequestDto;
+import com.cos.blog.repository.ReplyRepository;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,22 +26,35 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 public class Reply {
+	@Id //Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+	private int id; // 시퀀스, auto_increment
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)  //auto_Increment
-  private int id;
+	@Column(nullable = false, length = 200)
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name="boardId")
+	private Board board;
+	
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
+	
+	@CreationTimestamp
+	private LocalDateTime createDate;
 
-  @Column (nullable = false,  length =200)                                //대용량 데이터 다룰떄 사용 
-  private String content;                                                             //섬머노트 라이브러리 툴 사용:  <html> 태그가 섞여서 디자인 됨
-
-  @ManyToOne
-  @JoinColumn(name = "boardId")
-  private Board board;
-
-  @ManyToOne
-  @JoinColumn(name = "userId")
-  private User user;
-
-  @CreationTimestamp
-  private Timestamp createDate;
+	@Override
+	public String toString() {
+		return "Reply [id=" + id + ", content=" + content + ", board=" + board + ", user=" + user + ", createDate="
+				+ createDate + "]";
+	}
 }
+
+
+
+
+
+
+
+
